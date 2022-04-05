eb96f124231814352258298126020e73e447f923
KingMerek <therealkingmerek@gmail.com>
2022-04-05 17:43:10 -0600

    changed location of ze commentos

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 package Java;
-
+//this is a comment!//
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println( Hello world ) ;
-	//does this break something?//}
+	}
 }
