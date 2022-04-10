63df592e01422cb342b2e9ec3e7d9fa335f94f23
KingMerek <therealkingmerek@gmail.com>
2022-04-10 15:02:42 -0600

    I added 5 name variables, which are used for random attendents; I aslo added a fiew more sentences to the hello world, as well as a switch command for the five random names; I aslo added an Rattendent variable, which is the intagure that determains the random attendent, and a println and if else command to print the random intager picked.

--- Hello.java
+++ Hello.java
@@ -2,6 +2,48 @@ package Java;
 //this is a comment!//
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println( Hello world ) ;
+		String name = "John Snuff";
+		String name2 = "Cary Snuff";
+		String name3 = "John Arbuckle";
+		String name4 = "Kim Harvord";
+		String name5 = "Yaldock T. Fenor";
+		String ErrRobot = "A Robotic voice says";
+ 		int Rattendent = (int)(Math.random() * 5);
+
+
+
+		System.out.println("Hello, Welcom to the the IGSC. Here we ship materials all across the known galaxy. If you would like more information, see our attendent, " + name +".") ;
+		System.out.println(" ");
+
+		switch (Rattendent) {
+			case 0:
+				System.out.println(ErrRobot + " Says \"We are sorry sir but " + name + " is not present currently. We here at the IGSC hope that there is no inconvinence, plese try later, perhaps after lunch break.\"");
+				break;
+			case 1:
+				System.out.println(name + " says \"Hello my friend, how may I help you today?\"");
+				break;
+			case 2:
+				System.out.println(name2 + " says \"Hello my friend, how may I help you today?\"");
+				break;
+			case 3:
+				System.out.println(name3 + " says \"Hello my friend, how may I help you today?\"");
+				break;
+			case 4:
+				System.out.println(name4 + " says \"Hello my friend, how may I help you today?\"");
+				break;
+			case 5:
+				System.out.println(name5 + " says \"Hello my friend, how may I help you today?\"");
+		}
+
+		if (Rattendent > 1) {
+			System.out.println("R > 1");
+			} else {
+			System.out.println("R < 1");
+			}
+
+		System.out.println(Rattendent);
+		
 	}
-}
+}	
+
+//i dont understand what ascii is//
